f23c0bee0c6ee2e02b98ca6955b8630533068fa6
sketch337 <shreya.tadikamalla771@education.nsw.gov.au>
2026-05-25 16:55:22 +1000

    Needed to change my filetype due to watching another tutorial

--- shreya_task3.py
+++ shreya_task3.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-
+def click(event):
+    pass
 
 
 root = tk.Tk()
@@ -12,4 +13,22 @@ entry_var = tk.StringVar()
 
 entry = tk.Entry(root, textvar=entry_var, font='Arial 20', justify='right')
 entry.pack(fill='both', ipadx=8, pady=10, padx=10)
+
+button_frame = tk.Frame(root)
+button_frame.pack()
+
+buttons = [
+    ["7","8","9","/"],
+    ["4","5","6","*"],
+    ["1","2","3","-"],
+    ["0","C","=","+"]
+]
+
+for row in buttons:
+    frame = tk.Frame(button_frame)
+    frame.pack(expand=True, fill='both')
+    for btn in row:
+        b = tk.Frame(button_frame, text=btn, font='Arial 18', relief='ridge', width=5, height=2)
+        b.pack(side='left', expand=True, fill='both')
+        b.bind('<Button-1>', click)
 root.mainloop()
